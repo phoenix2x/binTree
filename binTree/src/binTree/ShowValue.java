dc6c2215acc1d69d787b060d4938bbed64b9f50c
phoenix2x <phoenixer@tut.by>
2014-02-27 10:54:12 +0300

    test2

--- binTree/src/binTree/ShowValue.java
+++ binTree/src/binTree/ShowValue.java
@@ -6,6 +6,6 @@ public class ShowValue<T extends Comparable<T>> implements Process<T> {
 
 	@Override
 	public void process(T value) {
-		System.out.println(value+"");
+		System.out.println(value);
 	}
 }
